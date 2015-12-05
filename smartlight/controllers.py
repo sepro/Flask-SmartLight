060009491e843e82eab5bcb1dbde05b8f0e3e13e
Sebastian Proost <sebastian.proost@gmail.com>
2015-12-05 10:03:05 +0100

    color can be set by name and rgb value, also there is a get color controller

--- smartlight/controllers.py
+++ smartlight/controllers.py
@@ -11,8 +11,8 @@ def index():
     return 'Hello World!'
 
 
-@main.route('/color/set/<name>')
-def set_color(name):
+@main.route('/color/name/<name>')
+def set_color_name(name):
     try:
         led.set_color(name=name)
     except Exception as e:
@@ -21,3 +21,24 @@ def set_color(name):
         return json.dumps({'status': 'succes', 'message': 'Set color to ' + name})
 
 
+@main.route('/color/rgb/<int:r>/<int:g>/<int:b>')
+def set_color_rgb(r, g, b):
+    try:
+        led.set_color(red=r, green=g, blue=b)
+    except Exception as e:
+        return json.dumps({'status': 'error', 'message': e})
+    else:
+        message = 'Set color to rgb %d, %d, %d' % (r, g, b)
+        return json.dumps({'status': 'succes', 'message': message})
+
+
+@main.route('/color/')
+@main.route('/color')
+def get_color_rgb():
+    try:
+        color = led.get_color()
+    except Exception as e:
+        return json.dumps({'status': 'error', 'message': e})
+    else:
+        message = 'Current color ' + str(color)
+        return json.dumps({'status': 'succes', 'message': message, 'color': color})
